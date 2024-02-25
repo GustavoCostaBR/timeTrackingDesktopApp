63b3e302aa67163c5816fb0bcb41c349f7df3023
Gustavo da Costa <gustavocosta.nh@gmail.com>
2024-02-25 12:08:02 -0300

    Controller

--- src/allogica/trackingTimeDesktopApp/controller/ActivityController.java
+++ src/allogica/trackingTimeDesktopApp/controller/ActivityController.java
@@ -1,5 +1,20 @@
 package allogica.trackingTimeDesktopApp.controller;
 
-public class ActivityController {
+import allogica.trackingTimeDesktopApp.model.entity.ActivityEnd;
+import allogica.trackingTimeDesktopApp.model.entity.ActivityStart;
 
+public class ActivityController implements IController {
+	
+	
+	private Activity activity;
+	private ActivityForm frame;
+	private ActivityEnd activityEnd;
+	private ActivityStart activityStart;
+	
+	public void executa(Object view) {
+		frame = (CalculoForm) view;
+		calculo = new Calculo();
+		
+	}
+	
 }
